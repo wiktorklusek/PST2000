1b25d7edb5e37e539506fdc1dd6927b4114ab4c0
wiktorklusek <63818570+wiktorklusek@users.noreply.github.com>
2023-04-18 13:53:57 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -7,6 +7,9 @@ namespace HelloWorld
     static void Main(string[] args)
     {
       Console.WriteLine("Hello World!");
+      Console.WriteLine("What is your name?");
+      string str = Console.ReadLine();
+      Console.WriteLine("Why, hello there " + str);
     }
   }
 }
